bdf7a15d91b19c2ad6e4e74aa29d42f894ccd2fc
ab-rehman1 <disclaimerface@gmail.com>
2025-05-10 21:56:04 -0400

    Update.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ const Form = ({ sideContent, sideClassName = "", formNextClass="" }) => {
         <>
             <section className={`FormPlus ${formNextClass}`}>
                 <div className={`sideFormSection ${sideClassName}`}>{sideContent}</div>
-                <form className="Form">
+                <form className="Form" name="Contact" netlify>
                     <span className="Formheading">
                         <h1>Let's <span>Work Together</span></h1>
                         <p>I'm here to help if you're searching for a product designer to bring your idea to life or a design partner to help take your business to the next level.</p>
@@ -18,14 +18,14 @@ const Form = ({ sideContent, sideClassName = "", formNextClass="" }) => {
                         </span>
                         <input type="email" name="email" placeholder="Email" required />
                         <input type="text" name="subject" placeholder="Subject" required />
-                        <select className="select" required defaultValue="">
+                        <select className="select" name="select" required defaultValue="">
                             <option className="options" value="" disabled>Select Budget</option>
                             <option className="options" value="$50">$50</option>
                             <option className="options" value="$50 - $100">$50 - $100</option>
                             <option className="options" value="$100 - $200">$100 - $200</option>
                             <option className="options" value="$200+">$200+</option>
                         </select>
-                        <textarea placeholder="Enter your Message.." ></textarea>
+                        <textarea placeholder="Enter your Message.." name="message" ></textarea>
                         <button type="submit" name="submit">Send Message <i className="ri-arrow-right-up-long-line"></i></button>
                     </div>
                 </form>
